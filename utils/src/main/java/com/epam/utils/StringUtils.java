43227ea5dae44dbc8a8100d4187746a77822c79e
elshodjava <126221796+elshodjava@users.noreply.github.com>
2023-02-24 09:51:04 +0500

    Update StringUtils.java

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,13 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         //code!
+        if (NumberUtils.isParsable(str)) {
+            return Double.compare(Double.parseDouble(str),0) > 0;
+        }
+        return false;
     }
 }
